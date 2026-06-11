c72c435081c0bacabfbb0596bf1bb2b94bd545f9
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-06-11 11:40:35 +0700

    menambahkan validasi tree kosong

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -73,4 +73,17 @@ public:
     }
 
     void inorder(Node *ptr)
- 
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
