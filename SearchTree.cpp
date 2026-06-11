3c5a333275d7cc823919c24a45e43bc0f3690786
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-06-11 11:34:52 +0700

    menambahkan node baru

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -28,6 +28,30 @@ public:
     }
 
     void insert(string element)
-};
+    {
+        Node *newNode = new Node(element, NULL, NULL);
 
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild= NULL;
 
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
